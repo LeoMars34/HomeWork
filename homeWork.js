e1a1a7bb5f05604c751fca3a326b9eb5df4b4e66
Олег Гавриков <leomars30stm@gmail.com>
2024-01-17 14:49:55 +0300

    complete

--- homeWork.js
+++ homeWork.js
@@ -1,3 +1,4 @@
+            //Лекция 3 ДЗ//
 
             //Задание 1//
 
@@ -51,3 +52,108 @@
                 return Array.from(str).reverse().join('');
             }
 
+            //Лекция 4 ДЗ//
+            //Задание 1//
+
+            Основная причина этого заключается в том, что массивы в JavaScript являются динамическими и могут содержать элементы разных типов данных.
+            Массивы могут содержать stack,queue, ассоциативные массивы или хеш-таблицы
+            
+            //Задание 2//
+
+            function logger() {
+                console.log(`I output only external context: ${this.item}`);
+            }
+            const obj = { item: 'some value' };
+            logger.call(obj);
+            logger.apply(obj);
+            const bindLogger = logger.bind(obj);
+            bindLogger();
+
+            //Бонус задание//
+
+            if (!Function.prototype.bind) {
+                Function.prototype.bind = function (context, ...args) {
+                  if (typeof this !== 'function') {
+                    throw new TypeError('Function.prototype.bind - что-то пошло не так');
+                  }
+                  let fn = this;
+                  return function (...boundArgs) {
+                    return fn.apply(context, args.concat(boundArgs));
+                  };
+                };
+              }
+
+              //Лекция 5 ДЗ//
+              //Задание 1//
+            Сортировка пузырьком
+            Сортировка выбором
+            Сортировка вставками
+            Сортировка слиянием
+            Быстрая сортировка
+            Блочная сортировка
+
+                //Задание 3//
+                const person = {
+                    age: 33,
+                    name: 'Oleg',
+                };
+                const person2 = (new Object().__proto__ = person);
+                person.logInfo = function () {};
+                //Задание 4//               
+                class Person {
+                    constructor(name) {
+                      this.name = name;
+                    }
+                  }       
+                  class PersonThree extends Person {
+                    constructor(name, age) {
+                      super(name);
+                      this.age = age;
+                    }                 
+                    get name() {
+                      return this._name;
+                    }     
+                    set name(value) {
+                      this._name = value;
+                    }
+                  }
+                  //Бонус//
+                  //1//
+                arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+                total = 13;
+                const firstSum = (arr, total) => {
+                let left = 0;
+                let right = arr.length - 1;
+
+                while (left < right) {
+                    const sum = arr[left] + arr[right];
+                    if (sum === total) {
+                        return [arr[left], arr[right]];
+                    } else if (sum < total) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            };
+            firstSum(arr, total);
+            //2//
+            Сложность - O(n)
+
+            //Лекция 6 ДЗ//
+            1. 1 3 6 4 5 2
+            2. 1 3 2
+            3. abc
+            4. 1 123,2 123,3 321,4 undefined
+            5. 1 4 3 2
+            
+            //Задание 6//
+             let arr = [10, 12, 15, 21];
+            arr.forEach((num, index) => {
+                setTimeout(() => {
+                    console.log(index);
+                }, index * 3000);
+            });
+           
+           
+            
